705c5de52fbb2fcf926eee8bbf47d540a2591b98
Bibi <84073518+xBibie@users.noreply.github.com>
2021-06-21 18:31:39 +0200

    Add files via upload

---
+++ opdracht-java/index.js
@@ -0,0 +1,27 @@
+ // This is my first JavaScript
+console.log('Hello World')    
+console.log let name = 'Bibi'
+console.log let firstName = 'if' 
+console.log let age = 28
+
+function myFunctionplus() {
+    var y = 5;
+    var x = y + 2;
+    document.getElementById("demo").innerHTML = x;
+  }
+
+  function myFunction() {
+    var y = 5;
+    var x = y - 2;
+    document.getElementById("demo").innerHTML = x;
+  }
+  function myFunctionMul() {
+    var y = 5;
+    var x = y * 2;
+    document.getElementById("demo").innerHTML = x;
+  }
+  console.log(typeof 42);
+// expected output: "number"
+
+console.log(typeof 'blubber');
+// expected output: "string"
